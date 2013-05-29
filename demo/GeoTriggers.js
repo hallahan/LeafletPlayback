8ec273627a4a8b691715397d42efeaaa76e0c3b1
Nicholas Hallahan <nick@theoutpost.io>
2013-05-29 16:27:37 -0700

    getting trigger history working

--- demo/GeoTriggers.js
+++ demo/GeoTriggers.js
@@ -7,9 +7,6 @@ function GeoTriggers(featureGroup, callback) {
     client_id: "d8e57ca4439b8ac89bc0be27c4ed9b8e"
   });
 
-  // this.triggerHistory = JSON.parse(window.localStorage.triggerHistory);
-  // if (!this.triggerHistory) this.triggerHistory = [];
-
   this.login();
   this.logLayers();
   this.logPlaces();
@@ -129,61 +126,32 @@ GeoTriggers.prototype.updateLocation = function() {
 }
 
 
-// GeoTriggers.prototype._poll = function() {
-//   var updated = false;
-//   var self = this;
-//   self.updateLocation();
-//   geoloqi.get("trigger/history", {}, function(res, err) {
-//     var updates = res.history;
-//     if (updates.length === 0) return;
-//     var len = self.triggerHistory.length;
-//     var newestStored = self.triggerHistory[len-1];
-//     var oldestUpdate = updates.pop();
-//     while (oldestUpdate && oldestUpdate.date_ts <= newestStored.date_ts ) {
-//       oldestUpdate = updates.pop();
-//     }
-//     while (oldestUpdate) {
-//       self._newTrigger(oldestUpdate);
-//       updated = true;
-//       oldestUpdate = updates.pop();
-//     }
-//     if (updated) self.persist();
+GeoTriggers.prototype._poll = function() {
+  var updated = false;
+  var self = this;
+  self.updateLocation();
+  geoloqi.get("trigger/history", {}, function(res, err) {
+    this.triggerHistory = res.history;
+    console.log(['triggerHistory', res.history]);
 
-//     // poll in a second again
-//     self._timeoutID = window.setTimeout(function(self) {
-//       self._poll();
-//     }, 1000, self);
-//   });
+    // poll timer
+    self._timeoutID = window.setTimeout(function(self) {
+      self._poll();
+    }, 1000, self);
+  });
   
-// }
+}
 
 
-// GeoTriggers.prototype.persist = function() {
-//   var json = JSON.stringify(this.triggerHistory);
-//   window.localStorage.triggerHistory = json;
-// }
+GeoTriggers.prototype.startPolling = function() {
+  if (this._timeoutID) return;
+  this._poll();
+}
 
 
-// GeoTriggers.prototype._newTrigger = function(trigger) {
-//   this.triggerHistory.push(trigger);
-//   this.callback(trigger);
-// }
-
-
-// GeoTriggers.prototype.startPolling = function() {
-//   if (this._timeoutID) return;
-//   this._poll();
-// }
-
-
-// GeoTriggers.prototype.stopPolling = function() {
-//   if (this._timeoutID) {
-//     window.clearTimeout(this._timeoutID);
-//     this._timeoutID = null;
-//   }
-// }
-
-
-// GeoTriggers.prototype.addTrigger = function(trigger) {
-
-// }
+GeoTriggers.prototype.stopPolling = function() {
+  if (this._timeoutID) {
+    window.clearTimeout(this._timeoutID);
+    this._timeoutID = null;
+  }
+}
