5f39edaad8522c4c9a30aa8f1b1922ba7679fac4
Nicholas Hallahan <nick@theoutpost.io>
2013-06-14 20:12:51 -0700

    separating the wheat from the chaff

--- demo/simple/simple.js
+++ demo/simple/simple.js
@@ -1,24 +1,9 @@
 $(function() {
-  $('#bottom-branding').on('click', function(e) {
-    $('header').fadeIn();
-  });
-
-  $('.close-header').on('click', function(e) {
-    $('header').fadeOut();
-  });
-
-  $('#play-demo-btn').on('click', function(e) {
-    playback.start();
-    geoTriggers.startPolling();
-    $('#play-pause-icon').removeClass('icon-play');
-    $('#play-pause-icon').addClass('icon-pause');
-    isPlaying = true;
-  });
 
   demoTracks = [blodgett, blueMountain, drive, houseToCoordley, tillicum];
 
 	//creates a new map
-	map = new L.Map('map', {zoomControl:false});
+	map = new L.Map('map');
 
   // var basemapURL = 'http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}';
 	var basemapURL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -41,38 +26,25 @@ $(function() {
 		}
 	});
 
-  geoTriggerFeatureGroup = new L.FeatureGroup();
-  geoTriggerFeatureGroup.addTo(map);
 	var l = {
-		'<i class="icon-bullseye"></i> GPS Tracks': samples,
-    '<i class="icon-bell"></i> Virtual Fences': geoTriggerFeatureGroup
+		'<i class="icon-bullseye"></i> GPS Tracks': samples
 	};
 
 	L.control.layers(null, l, {
-    collapsed: false,
-    position: 'topleft'
+    collapsed: false
   }).addTo(map);
 
-  L.control.scale({metric:false}).addTo(map);
-
 	playback = new L.Playback(map, demoTracks, clockCallback);
-
-	map.on('click', function(e) {
-    console.log(e.latlng.toString());
-	});
-
 
 	isPlaying = false;
 	$('#play-pause').click(function() {
 		if (isPlaying === false) {
 			playback.start();
-    	geoTriggers.startPolling();
     	$('#play-pause-icon').removeClass('icon-play');
     	$('#play-pause-icon').addClass('icon-pause');
     	isPlaying = true;
 		} else {
 			playback.stop();
-    	geoTriggers.stopPolling();
     	$('#play-pause-icon').removeClass('icon-pause');
     	$('#play-pause-icon').addClass('icon-play');
     	isPlaying = false;
@@ -170,102 +142,9 @@ $(function() {
     $('#load-tracks-modal').modal();
   });
 
-	// Initialize the draw control and pass it the FeatureGroup of editable layers
-	drawControl = new L.Control.Draw({
-		draw: {
-			position: 'topleft',
-			polyline: false,
-			polygon: false,
-			rectangle: false,
-			marker: false,
-			circle: {
-				title: "Create a Virtual Fence!",
-				shapeOptions: {
-					color: '#662d91'
-				}
-			}
-		},
-		edit: {
-			featureGroup: geoTriggerFeatureGroup
-		}
-	});
-	map.addControl(drawControl);
-
-	map.on('draw:created', function (e) {
-		var type = e.layerType
-			, layer = e.layer;
-
-		if (type === 'marker') {
-			layer.bindPopup('A popup!');
-		}
-
-    if (type === 'circle') {
-      var latlng = layer.getLatLng();
-      $('#new-trigger-lat').html(latlng.lat);
-      $('#new-trigger-lng').html(latlng.lng);
-      var radius = layer.getRadius();
-      $('#new-trigger-radius').html(radius);
-
-      $('input, textarea').val('').html('');
-      $('#create-geotrigger-modal').modal();
-    }
-
-		// geoTriggerFeatureGroup.addLayer(layer);
-	});
-
-	map.on('draw:edited', function (e) {
-		var layers = e.layers;
-		var countOfEditedLayers = 0;
-		layers.eachLayer(function(layer) {
-			countOfEditedLayers++;
-
-			geoTriggers.editTrigger({
-				latlng: layer.getLatLng(),
-				radius: layer.getRadius(),
-				placeId: layer.placeId
-			});
-
-		});
-		console.log("Edited " + countOfEditedLayers + " layers");
-
-	});
-
-	map.on('draw:deleted', function(e) {
-		e.layers.eachLayer(function(layer) {
-			geoTriggers.deleteTrigger(layer.placeId);
-		});
-	});
-
-	// NH TODO this doesnt work...
-	// L.DomUtil.get('changeColor').onclick = function () {
-	// 	drawControl.setDrawingOptions({ rectangle: { shapeOptions: { color: '#004a80' } } });
-	// };
-
-  geoTriggers = new GeoTriggers(geoTriggerFeatureGroup, triggerFired);
-
-  $('#create-geotrigger-save').on('click', function(e) {
-
-  	geoTriggers.createTrigger({
-  		lat: parseFloat($('#new-trigger-lat').html()),
-  		lng: parseFloat($('#new-trigger-lng').html()),
-  		radius: parseFloat($('#new-trigger-radius').html()),
-  		name: $('#new-trigger-name').val(),
-  		message: $('#new-trigger-message').val()
-  	});
-
-  	console.log('save');
-  	$('#create-geotrigger-modal').modal('hide');
-  });
-
-
   $('#load-tracks-save').on('click', function(e) {
     var file = $('#load-tracks-file').get(0).files[0];
     loadTracksFromFile(file);
-  });
-
-  $('#view-all-fences-btn').on('click', function(e) {
-    var bounds = geoTriggerFeatureGroup.getBounds();
-    map.fitBounds(bounds);
   });
 
 });
@@ -274,7 +153,6 @@ function clockCallback(ms) {
 	$('#cursor-date').html(L.Playback.Util.DateStr(ms));
 	$('#cursor-time').html(L.Playback.Util.TimeStr(ms));
 	$('#time-slider').slider('value', ms);
-  // $('#timepicker').timepicker('setTime', new Date(ms).toTimeString());
 }
 
 function speedToSliderVal(speed) {
@@ -285,35 +163,6 @@ function speedToSliderVal(speed) {
 function sliderValToSpeed(val) {
 	if (val < 0) return parseFloat((1+val/10).toFixed(2));
 	return val + 1;
-}
-
-function triggerFired(trigger) {
-  console.log('triggerFired');
-  console.log(trigger);
-
-  var html = 
-'<div class="accordion-inner">' +
-'  <strong>'+trigger.place.name+'</strong>' +
-'  <span class="broadcast-time">'+ trigger.display_date + '</span>' +
-'  <br/>' +
-   trigger.trigger.text + '<br/>' +
-'  <button class="btn btn-info btn-small view-notification"><i class="icon-eye-open"></i> View</button>'+
-'</div>';
-
-  $('#notifications').prepend(html);
-  var count = $('#notifications').children().length;
-  $('#notification-count').html('<span class="badge badge-important pull-right">'+count+'</span>');
-  var $btn = $('#notifications').find('button').first();
-  $btn.data('trigger',trigger);
-  $btn.on('click', function(e) {
-    console.log('view trigger');
-    var lat = trigger.place.latitude;
-    var lng = trigger.place.longitude;
-    var radius = trigger.place.radius * 1.5;
-    var circle = new L.Circle([lat,lng],radius);
-    var bounds = circle.getBounds();
-    map.fitBounds(bounds);
-  });
 }
 
 function combineDateAndTime(date, time) {
@@ -337,26 +186,4 @@ function loadTracksFromFile(file) {
     samples.addData(tracks);
     $('#load-tracks-modal').modal('hide');
   }
-}
-
-function save(data, name) {
-  var json = JSON.stringify(data, null, 2);
-  var blob = new Blob([json], {type:'text/plain'});
-  var downloadLink = document.createElement("a");
-  var url = (window.webkitURL != null ? window.webkitURL : window.URL);
-  downloadLink.href = url.createObjectURL(blob);
-  downloadLink.download = name || 'data.json';
-  downloadLink.click();   
-}
-
-function sliceData(data, start,end) {
-  end = end || data.geometry.coordinates.length-1;
-  data.geometry.coordinates = data.geometry.coordinates.slice(start,end);
-  data.properties.time = data.properties.time.slice(start,end);
-  data.properties.speed = data.properties.speed.slice(start,end);
-  data.properties.altitude = data.properties.altitude.slice(start,end);
-  data.properties.heading = data.properties.heading.slice(start,end);
-  data.properties.horizontal_accuracy = data.properties.horizontal_accuracy.slice(start,end);
-  data.properties.vertical_accuracy = data.properties.vertical_accuracy.slice(start,end);
-  save(data,'sliced-data.json');
 }
