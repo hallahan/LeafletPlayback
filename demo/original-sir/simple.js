0e70d8ba7bd8e95feb4864c93bce322742605e8d
Nicholas Hallahan <nick@theoutpost.io>
2013-08-19 17:39:47 -0700

    original sir trip now animates

---
+++ demo/original-sir/simple.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  //creates a new map
+  map = new L.Map('map');
+
+  var basemapURL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var basemapLayer = new L.TileLayer(basemapURL, {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>',
+  	maxZoom : 19
+  });
+
+  // centers map and default zoom level
+  map.setView([44, -99], 5);
+
+  // adds the background layer to the map
+  map.addLayer(basemapLayer);
+
+  // I made playback global so you can call methods on it
+  // from the console. You can leave out the second argument
+  // if you do not want to preload tracks.
+  playback = new L.Playback(map, demoTracks);
+
+});
