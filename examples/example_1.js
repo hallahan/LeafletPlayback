247c10ac651c422592fd1ffb2cc70a8d16422c56
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-02-02 21:50:31 -0800

    added cname

--- examples/example_1.js
+++ examples/example_1.js
@@ -27,7 +27,7 @@ $(function() {
     var basemapLayer = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/github.map-xgq2svrz/{z}/{x}/{y}.png');
 
     // Center map and default zoom level
-    map.setView([44.61131534, -123.4726739], 9);
+    map.setView([44.5, -123.6], 10);
 
     // Adds the background layer to the map
     map.addLayer(basemapLayer);
